783cef078794ca76331bfa22554971b751264d64
noo-min-555 <noo_min-555@hotmail.com>
2013-11-24 00:40:50 -0800

    Project
    Project ใส่ชื่อจับเวลา

---
+++ pigwheel/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace pigwheel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(104, 87);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(233, 173);
+            this.listBox1.TabIndex = 23;
+            this.listBox1.Tag = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(166, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 57);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "จับเวลา";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(270, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 33);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(215, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 33);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(161, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 33);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(107, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 33);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "00";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label7.ForeColor = System.Drawing.Color.Blue;
+            this.label7.Location = new System.Drawing.Point(201, 49);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 33);
+            this.label7.TabIndex = 21;
+            this.label7.Text = ":";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label6.ForeColor = System.Drawing.Color.Blue;
+            this.label6.Location = new System.Drawing.Point(256, 49);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 33);
+            this.label6.TabIndex = 20;
+            this.label6.Text = ":";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label5.ForeColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(148, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 33);
+            this.label5.TabIndex = 19;
+            this.label5.Text = ":";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(291, 274);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 28;
+            this.button5.Text = "รีเซ็ต";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 277);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 20);
+            this.textBox1.TabIndex = 27;
+            this.textBox1.TabStop = false;
+            this.textBox1.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(37, 316);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 57);
+            this.button4.TabIndex = 26;
+            this.button4.Text = "หยุด";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(291, 316);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 57);
+            this.button3.TabIndex = 25;
+            this.button3.Text = "ออก";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(147, 264);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 42);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "เปลี่ยนคำนำหน้า";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 397);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ pigwheel/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pigwheel
+{
+
+    public partial class Form1 : Form
+    {
+        int i, i2, i3, i4, i5;
+        string k1, k2, k3, k4;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public Form1(string kk)
+        {
+            k4 = kk;
+
+
+            InitializeComponent();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            k1 = i2.ToString();
+            k2 = i3.ToString();
+            k3 = i4.ToString();
+            if (timer1.Interval == 10)
+            {
+                i = i + 1;
+                label4.Text = i.ToString();
+            }
+            if (i == 100)
+            {
+                i2 = i2 + 1;
+                i = 0;
+                label3.Text = i2.ToString();
+            }
+            if (i2 == 59)
+            {
+                i3 = i3 + 1;
+                i2 = 0;
+                label2.Text = i3.ToString();
+            }
+            if (i3 == 59)
+            {
+                i4 = i4 + 1;
+                i3 = 0;
+                label1.Text = i4.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button4.Enabled = true;
+            button5.Enabled = false;
+            if (button1.Text == "จับเวลา")
+            {
+                button1.Text = "เก็บ";
+                timer1.Enabled = true;
+            }
+            else if (button1.Text == "เก็บ")
+            {
+                i5 = i5 + 1;
+                listBox1.Items.Add(k4 + "   " + i5 + "   " + k3 + ":" + k2 + ":" + k1);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "เปลี่ยนคำนำหน้า")
+            {
+                textBox1.Text = "";
+                button1.Enabled = false;
+                button2.Text = "ตกลง";
+                textBox1.Visible = true;
+                textBox1.TabStop = true;
+            }
+            else
+            {
+                button1.Enabled = true;
+                button2.Text = "เปลี่ยนคำนำหน้า";
+                textBox1.Visible = false;
+                textBox1.TabStop = false;
+                k4 = textBox1.Text;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button1.Text = "จับเวลา";
+            timer1.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = true;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            i2 = 0;
+            i3 = 0;
+            i4 = 0;
+            i5 = 0;
+            label1.Text = "00";
+            label2.Text = "00";
+            label3.Text = "00";
+            label4.Text = "00";
+            listBox1.Items.Clear();
+        }
+    }
+}
